1df9d4902055890ce9e8251f72e556fbefacb606
anatoly <tolitius@gmail.com>
2016-02-24 00:39:33 -0500

    merging with ENV to sprinkle "12 factor"
    + better container support (cloud foundry / kubernetes / etc..)

--- src/cprop/cprop.clj
+++ src/cprop/cprop.clj
@@ -33,12 +33,53 @@
 
 (defonce ^:private props (atom {}))
 
-(defn load-config 
-  ([] (load-config nil))
-  ([path] (reset! props (slurp-props path))))
-
 (defn conf [& path]                  ;; e.g. (conf :datomic :url)
   (get-in @props (vec path)))
+
+(defn- env->path [v]
+  (as-> v $
+        (s/lower-case $)
+        (s/replace $ "__" "-")
+        (s/split $ #"_")
+        (map keyword $)))
+
+(defn- read-system-env []
+  (->> (System/getenv)
+       (map (fn [[k v]] [(env->path k) v]))
+       (into {})))
+
+(defn- typinize [k v]
+  (try 
+    (edn/read-string v)
+    (catch Throwable problem
+      (println "could not read value:" v "for key:" k "due to:" (.getMessage problem) ". casting it to string")
+      (str v))))
+
+;; merge with ENV, system props, etc..
+
+(defn- substitute [m [k-path v]]
+  (if (and (seq k-path) (get-in m k-path))
+    (do
+      ;; (println "substituting" (vec k-path) "with" v)
+      (println "substituting" (vec k-path) "with a ENV/system.property specific value")
+      (assoc-in m k-path (edn/read-string v)))
+    m))
+
+(defn- merge* [config with]
+  (reduce substitute config with))
+
+;; entry point
+
+(defn load-config 
+  ([] (load-config nil))
+  ([path] 
+   (let [config (slurp-props path)
+         env (read-system-env)
+         sys-props {}]        ;; TODO system properties
+     (reset! props (-> config
+                       (merge* env))))))
+
+;; cursors
 
 (defn- create-cursor [path]
   (with-meta 
--- test/cprop/core_test.clj
+++ test/cprop/core_test.clj
@@ -1,5 +1,6 @@
 (ns cprop.core-test
   (:require [cprop :refer [conf cursor load-config]]
+            [clojure.edn :as edn]
             [clojure.test :refer :all]))
 
 (load-config)
@@ -19,6 +20,38 @@
     (is (= ((cursor (cursor :source) :account) :rabbit :vhost) "/z-broker")))
   (testing "should compose nested cursors"
     (is (= ((cursor (cursor (cursor :source) :account) :rabbit) :vhost) "/z-broker"))))
+
+(defn- read-system-env []
+  (->> {"DATOMIC_URL" "\"datomic:sql://?jdbc:postgresql://localhost:5432/datomic?user=datomic&password=datomic\""
+        "AWS_ACCESS__KEY" "\"AKIAIOSFODNN7EXAMPLE\""
+        "AWS_SECRET__KEY" "\"wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY\""
+        "AWS_REGION" "\"ues-east-1\""
+        "IO_HTTP_POOL_CONN__TIMEOUT" "60000"
+        "IO_HTTP_POOL_MAX__PER__ROUTE" "10"
+        "OTHER__THINGS" "[1 2 3 \"42\"]"}
+       (map (fn [[k v]] [(#'cprop/env->path k) v]))
+       (into {})))
+
+(deftest should-merge-with-env
+  (let [config (edn/read-string 
+                 (slurp "test/resources/fill-me-in.edn"))]
+    (is (= {:datomic {:url "datomic:sql://?jdbc:postgresql://localhost:5432/datomic?user=datomic&password=datomic"},
+            :aws {:access-key "AKIAIOSFODNN7EXAMPLE",
+                  :secret-key "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY",
+                  :region "ues-east-1",
+                  :visiblity-timeout-sec 30,
+                  :max-conn 50,
+                  :queue "cprop-dev"},
+            :io
+            {:http
+             {:pool
+              {:socket-timeout 600000,
+               :conn-timeout 60000,
+               :conn-req-timeout 600000,
+               :max-total 200,
+               :max-per-route 10}}},
+            :other-things [1 2 3 "42"]}
+           (#'cprop/merge* config (read-system-env))))))
 
 (comment ;; playground
 
