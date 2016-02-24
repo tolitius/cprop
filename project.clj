d9aac48c880b6091585f69feb008b9556b042f6b
anatoly <tolitius@gmail.com>
2016-02-24 11:49:57 -0500

    on to clojure 1.8.0

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
 
-  :dependencies [[org.clojure/clojure "1.6.0"]]
+  :dependencies [[org.clojure/clojure "1.8.0"]]
             
   :source-paths ["src" "src/cprop"]
             
