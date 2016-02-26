82b7422e07e45cdc23d4fee35c3214247ccdfc89
anatoly <tolitius@gmail.com>
2016-02-25 23:13:53 -0500

    releasing 0.1.4

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject cprop "0.1.3-SNAPSHOT"
+(defproject cprop "0.1.4"
   :description "where all configuration properties converge"
   :url "https://github.com/tolitius/cprop"
   :license {:name "Eclipse Public License"
