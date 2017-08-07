f06f7f79b82b93c2d584c56445650eb9b6fd204c
anatoly <tolitius@gmail.com>
2017-08-06 23:41:08 -0400

    removing project.clj. boot is "up and only"

--- project.clj
+++
@@ -1,9 +0,0 @@
-(defproject  cprop "0.1.10-SNAPSHOT"
-  :dependencies
-  [[org.clojure/clojure "1.8.0"]
-   [boot/core "2.5.1" :scope "provided"]
-   [adzerk/bootlaces "0.1.13" :scope "test"]
-   [adzerk/boot-test "1.0.6" :scope "test"]
-   [tolitius/boot-check "0.1.1" :scope "test"]]
-  :source-paths
-  ["src"])
